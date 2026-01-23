11c897a018990f55a01d2450f884064ce748a846
hlabiad05 <hamzalabiad500@gmail.com>
2026-01-23 18:48:05 +0100

    Add files via upload

---
+++ system des etudiants/controle.py
@@ -0,0 +1,11 @@
+#exercice1
+N = int(input("Entrez un nombre entier N : "))
+
+somme = 0
+for i in range(1, N + 1):
+    somme += i
+
+print("La somme des", N, "premiers entiers est :", somme)
+
+
+
